73b6eb1d962fcb0d9e30825c79f0dfa4b1a4b041
victor-tucci <victor.tucci@beldex.io>
2022-11-18 19:00:13 +0530

    changes foe build cmake

--- bin/update_submodules.sh
+++ bin/update_submodules.sh
@@ -4,7 +4,7 @@
 git submodule update --init --force --recursive
 
 # update monero-project
-cd ./external/monero-project
-git checkout master
-git pull --ff-only origin master
+cd ./external/beldex-core
+git checkout beldex_cpp
+git pull --ff-only origin beldex_cpp
 cd ../../
